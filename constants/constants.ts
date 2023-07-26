c22335b601d70395416936a0a9eba3c3c89f88bc
karthik <geokarthikcb@gmail.com>
2023-07-26 15:00:16 +0530

    done

---
+++ constants/constants.ts
@@ -0,0 +1,21 @@
+export const constantSkills: {
+  [key: string]: string;
+} = {
+  HTML: "/assets/techskills/html.svg",
+  CSS: "/assets/techskills/css.svg",
+  Javascript: "/assets/techskills/javascript.svg",
+  ReactJs: "/assets/techskills/react.svg",
+  NextJs: "/assets/techskills/NextJs.svg",
+  Mongodb: "/assets/techskills/mongodb.svg",
+  NodeJs: "/assets/techskills/node.svg",
+  Python: "/assets/techskills/python.svg",
+  Java: "/assets/techskills/java.svg",
+};
+
+export const constantPlaygrounds: {
+  [key: string]: string;
+} = {
+  "HTML/CSS": "/assets/techskills/html.svg",
+  "ReactJs/Tailwind": "/assets/techskills/react.svg",
+ "NodeJs/Mongodb": "/assets/techskills/node.svg",
+};
